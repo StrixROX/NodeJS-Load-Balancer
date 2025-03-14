a4a65b9a497e8a1250bd5ca90a56ea66d759f49f
StrixROX <40167899+StrixROX@users.noreply.github.com>
2025-03-14 19:59:53 +0530

    Fixed tests for createEchoServer

--- src/utils/__tests__/createEchoServer.test.js
+++ src/utils/__tests__/createEchoServer.test.js
@@ -1,5 +1,15 @@
 const createEchoServer = require("../createEchoServer.js");
 
+const server = createEchoServer({
+  serverId: 1,
+  hostname: "localhost",
+  ip: "127.0.0.1",
+  port: 3000,
+});
+
+beforeAll(() => server.start());
+afterAll(() => server.close());
+
 describe("createEchoServer", () => {
   it("Throws error when invalid args are passed", () => {
     expect(() => createEchoServer()).toThrow("serverArgs was not passed");
@@ -19,83 +29,80 @@ describe("createEchoServer", () => {
     ).toThrow("port is required");
   });
 
-  const server = createEchoServer({
-    serverId: 1,
-    hostname: "localhost",
-    ip: "127.0.0.1",
-    port: 3000,
+  it("Throws Error 400 when request method is not POST", () => {
+    return new Promise(async (resolve, reject) => {
+      await fetch(`http://${server.hostname}:${server.port}`, {
+        method: "GET",
+      })
+        .then((response) => {
+          expect(response.status).toBe(400);
+          expect(response.statusText).toBe("Bad Request");
+        })
+        .catch((error) => reject(error));
+
+      await fetch(`http://${server.hostname}:${server.port}`, {
+        method: "PUT",
+        body: "helloworld",
+      })
+        .then((response) => {
+          expect(response.status).toBe(400);
+          expect(response.statusText).toBe("Bad Request");
+        })
+        .catch((error) => reject(error));
+
+      await fetch(`http://${server.hostname}:${server.port}`, {
+        method: "PATCH",
+        body: "helloworld",
+      })
+        .then((response) => {
+          expect(response.status).toBe(400);
+          expect(response.statusText).toBe("Bad Request");
+        })
+        .catch((error) => reject(error));
+
+      await fetch(`http://${server.hostname}:${server.port}`, {
+        method: "DELETE",
+        body: "helloworld",
+      })
+        .then((response) => {
+          expect(response.status).toBe(400);
+          expect(response.statusText).toBe("Bad Request");
+        })
+        .catch((error) => reject(error));
+
+      resolve();
+    });
   });
 
-  server.start();
-
-  it("Throws Error 400 when request method is not POST", async () => {
-    await fetch(`http://${server.hostname}:${server.port}`, {
-      method: "GET",
-    }).then((response) => {
-      expect(response.status).toBe(400);
-      expect(response.statusText).toBe("Bad Request");
-    });
-
-    await fetch(`http://${server.hostname}:${server.port}`, {
-      method: "PUT",
-      body: "helloworld",
-    }).then((response) => {
-      expect(response.status).toBe(400);
-      expect(response.statusText).toBe("Bad Request");
-    });
-
-    await fetch(`http://${server.hostname}:${server.port}`, {
-      method: "PATCH",
-      body: "helloworld",
-    }).then((response) => {
-      expect(response.status).toBe(400);
-      expect(response.statusText).toBe("Bad Request");
-    });
-
-    await fetch(`http://${server.hostname}:${server.port}`, {
-      method: "DELETE",
-      body: "helloworld",
-    }).then((response) => {
-      expect(response.status).toBe(400);
-      expect(response.statusText).toBe("Bad Request");
-    });
-  });
-
-  it("Streams data back to client", async () => {
-    await fetch(`http://${server.hostname}:${server.port}`, {
+  it("Streams data back to client", () => {
+    return fetch(`http://${server.hostname}:${server.port}`, {
       method: "POST",
       body: "helloworld",
-    }).then(async (response) => {
-      expect(response.status).toBe(200);
-      expect(response.headers.get("Content-Type")).toBe("text/plain");
-      expect(response.headers.get("Transfer-Encoding")).toBe("chunked");
+    })
+      .then(async (response) => {
+        expect(response.status).toBe(200);
+        expect(response.headers.get("Content-Type")).toBe("text/plain");
+        expect(response.headers.get("Transfer-Encoding")).toBe("chunked");
 
-      const reader = response.body.getReader();
+        const reader = response.body.getReader();
 
-      expect(reader).not.toBeNull();
+        const textDecoder = new TextDecoder("UTF-8");
 
-      const textDecoder = new TextDecoder("UTF-8");
+        // next chunks are the data
+        let data = "";
+        let keepReading = true;
+        while (keepReading) {
+          await reader.read().then(({ done, value }) => {
+            const res = textDecoder.decode(value);
+            data += res;
+            keepReading = !done;
+          });
+        }
 
-      // first chunk is server info
-      await reader.read().then(({ value }) => {
-        const res = textDecoder.decode(value);
-        expect(res).toBe(`[server #${server.id}] I Received: `);
+        return data;
+      })
+      .then((data) => {
+        expect(data).toBe(`[server #${server.id}] I Received: helloworld`);
       });
-
-      // next chunks are the data
-      let data = "";
-      let keepReading = true;
-      while (keepReading) {
-        await reader.read().then(({ done, value }) => {
-          const res = textDecoder.decode(value);
-          data += res;
-          keepReading = !done;
-        });
-      }
-      expect(data).toBe("helloworld");
-    });
-
-    // close the server at the end of the test
-    server.close();
   });
 });
